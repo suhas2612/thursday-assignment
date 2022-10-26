4c3792ffc1c71d38be06aebfaa064b3861872c07
suhas2612 <116726813+suhas2612@users.noreply.github.com>
2022-10-26 17:40:53 +0530

    Add files via upload

---
+++ Arrayequality.java
@@ -0,0 +1,12 @@
+package sss;
+import java.util .Arrays;
+public class Arrayequality {
+
+	public static void main(String[] args) {
+	         int[] arr1=new int[] {15,23,-12,20,34};
+	         int[] arr2=new int[] {15,23,-12,20,34};
+	         System.out.println(Arrays.equals(arr1 , arr2));
+	         
+	}
+
+}
