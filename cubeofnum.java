d9c56872e57b2565f282e1980585ec6982399cf4
suhas2612 <116726813+suhas2612@users.noreply.github.com>
2022-10-29 22:18:11 +0530

    Add files via upload

---
+++ cubeofnum.java
@@ -0,0 +1,29 @@
+package co;
+import java.util.*;
+
+public class cubeofnum {
+
+	public static void main(String[] args) {
+
+		int n,result;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		n = sc.nextInt();
+		sc.close();
+		for(int i=0; i<=n; i++) {
+		
+		result = i*i*i;
+		
+		
+		System.out.println(result);
+		
+		}
+	
+		
+		
+		
+	}
+
+	}
+
+
