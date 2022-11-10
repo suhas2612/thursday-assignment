15879346b27fd4ae8cbdbe20cdb9627651cc97d0
suhas2612 <116726813+suhas2612@users.noreply.github.com>
2022-11-10 10:45:48 +0530

    Add files via upload

---
+++ membermain.java
@@ -0,0 +1,16 @@
+package com.java.Inheritance;
+
+
+	public class MemberMain {
+
+		public static void main(String[] args) {
+			Managers ms = new Managers();
+			System.out.println("enter details:");
+			ms.setDetails();
+			System.out.println("Details are");
+			ms.getDetails();
+		}
+
+	}
+
+
