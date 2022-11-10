a057c2fb39bc67555ea2f3bb6cf5c127cfd65be8
suhas2612 <116726813+suhas2612@users.noreply.github.com>
2022-11-10 11:04:06 +0530

    Add files via upload

---
+++ Rectanglemain.java
@@ -0,0 +1,19 @@
+package rt;
+
+public class Rectanglemain {
+      
+    	  public static void main(String[] args) {
+    			/*Rectangle r = new Rectangle();
+    			r.area(7, 4);*/
+    			//r.displayArea();
+    			/*r.perimetre(7, 4);*/
+    			//r.displayPerimeter();
+    			Square sq = new Square();
+    			sq.area(2, 3);
+    			sq.perimetre(2, 3);
+    			sq.perimeterOfSquare(3);
+    			sq.areaOfSquare(3);
+
+
+      }
+}
