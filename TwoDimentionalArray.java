5df5b63ba875d5cad516160742d7318af8a9405d
suhas2612 <116726813+suhas2612@users.noreply.github.com>
2022-11-19 14:26:18 +0530

    Add files via upload

---
+++ TwoDimentionalArray.java
@@ -0,0 +1,20 @@
+package mm;
+
+public class TwoDimentionalArray {
+	public class TowDimentionalArray {
+
+		public static void main(String[] args) {
+			int a[][]= {{10,20,30},{1,2,3}};
+				for(int i=0;i<2;i++)
+			{
+				for(int j=0;j<=2;j++)
+				{
+					System.out.print(a[i][j]+" ");
+				}
+			System.out.println();
+			}
+		}
+
+	}
+	
+}
