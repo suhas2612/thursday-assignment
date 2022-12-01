471e1842f18b26a9f983dcbe2dde3d279a1c9156
suhas2612 <116726813+suhas2612@users.noreply.github.com>
2022-12-01 21:56:51 +0530

    Add files via upload

---
+++ book.java
@@ -0,0 +1,17 @@
+package book;
+	
+
+
+public class Book {
+   String title;
+   String author;
+   double price;
+public Book(String title, String author, double price) {
+	super();
+	this.title = title;
+	this.author = author;
+	this.price = price;
+}
+   
+   
+}
